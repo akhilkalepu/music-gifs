65d9b3483687954cbcf88a07699abfbbe42c1ccb
theinfamousAK <akhilkalepu@protonmail.com>
2018-09-11 16:40:25 -0400

    initial button load and search bar complete. working on topicBtns and searchGiphy functions.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,39 @@
 $(document).ready(function () {
-// ------------------------------------------------
-var topics = ["dj", "guitar", "drums", "singer", "rapper", "synthesizer"]
+// ------------------------Variables------------------------
+var topics = ["dj", "guitar", "drums", "singer", "rapper", "synthesizer", "violin", "viola", "cello", "double bass", "trumpet", "saxaphone", "tuba", "french horn", "piano", "conductor"];
+
+var searchTopic = "";
+
+var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC";
+
+
+// ------------------------Topic Buttons------------------------
+topicBtns();
+
+function topicBtns() {
+    var i;
+    for (i = 0; i < topics.length; i++) {
+        $("#buttons-section").append('<button type="button" class="btn btn-dark topic-btn">' + topics[i] + '</button>')
+    }
+}
+
+// ------------------------Add Button------------------------
+$("#submitBtn").on("click", function() {
+    var searchTopic = $("#searchbar").val();
+    $("#buttons-section").empty();
+    topics.push(searchTopic);
+    topicBtns();
+    alert(searchTopic);
+});
+
+function searchGiphy() {
+    $.ajax({
+        url: queryURL,
+         method: "GET"
+    }).then(function(response) {
+        console.log(response);
+    });
+}
 
 // ------------------------------------------------
 });
